2ef9f6a4d51ecf2af31eb9726a210abb144518de
Joshua De Vera <deverajoshua02@gmail.com>
2025-02-16 00:28:30 +0800

    added auth contexts

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,11 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import UpcomingEvents from "./components/UpcomingEvents";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/auth/Login"; // Import Login Page
-import AdminDashboard from "./pages/AdminDashboard"; // Import Admin Dashboard
-import AlumniDashboard from "./pages/AlumniDashboard"; // Import Admin Dashboard
+import { AuthProvider } from "./contexts/AuthContexts";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
-    <div className="font-sans bg-gray-100 min-h-screen">
-      <Router>
-        <Routes>
-          <Route path="/" element={<>
-            <Navbar isAdminPage={false} /> {/* Landing Page Navbar */}
-            <Hero />
-            <UpcomingEvents />
-          </>} />
-          <Route path="/login" element={<>
-            <Navbar isAdminPage={false} /> {/* Landing Page Navbar */}
-            <Login /></>
-          } />
-          {/* Protected Routes */}
-          <Route
-            path="/admin-dashboard"
-            element={
-              <AdminDashboard />
-            }
-          />
-
-          <Route
-            path="/alumni-dashboard"
-            element={
-              <AlumniDashboard />
-            }
-          />
-        </Routes>
-      </Router>
-    </div>
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
   );
 };
 
---
+++ src/contexts/AuthContexts.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    const login = (userData) => {
+        setUser(userData);
+        localStorage.setItem("user", JSON.stringify(userData));
+    };
+
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("user");
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => useContext(AuthContext);
