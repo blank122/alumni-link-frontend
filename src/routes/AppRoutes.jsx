2afc1083f7d1fc5a106272d30f0004c4016f34ee
Joshua De Vera <deverajoshua02@gmail.com>
2025-02-16 00:22:51 +0800

    added app routes for admin

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "../pages/admin/DashboardAdmin";
+import Products from "../pages/admin/Products";
+import Profile from "../pages/admin/Profile";
+import Orders from "../pages/admin/Orders";
+import Settings from "../pages/admin/Settings";
+
+
+import Login from "../pages/Login";
+// import AdminLayout from "../layouts/AdminLayout";
+import AuthLayout from "../layouts/AuthLayout";
+import ProtectedRoute from "./ProtectedRoutes";
+import { NotFound, AccessDenied, ServerError } from "../pages/errors/ErrorPage";
+import Home from "../pages/Home";
+import AdminLayout from "../layouts/AdminLayout";
+
+const AppRoutes = () => {
+
+    const user = {
+        isAuthenticated: true, // Change this based on auth logic
+        role: "user", // Example: "admin", "user"
+    };
+
+    return (
+        // <Router>
+        <Routes>
+            {/* Auth Routes */}
+            <Route element={<AuthLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+            {/* User route */}
+            {/* <Route element={<ProtectedRoute />}>
+                <Route path="/user" element={<UserLayout />}>
+                    <Route path="dashboard" element={<DashboardUser />} />
+                    <Route path="products" element={<ProductsUser />} />
+                    <Route path="orders" element={<OrdersUser />} />
+                    <Route path="profile" element={<ProfileUser />} />
+                    <Route path="settings" element={<SettingsUser />} />
+                </Route>
+            </Route> */}
+
+            {/* Admin Route */}
+            <Route element={<ProtectedRoute />}>
+                <Route path="/admin" element={<AdminLayout />}>
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="products" element={<Products />} />
+                    <Route path="orders" element={<Orders />} />
+                    <Route path="profile" element={<Profile />} />
+                    <Route path="settings" element={<Settings />} />
+                </Route>
+            </Route>
+
+        </Routes>
+        // </Router>
+    );
+};
+
+export default AppRoutes;
