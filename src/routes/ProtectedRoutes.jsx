9832d0cd5326736c8a1c0beb7f125144bed29e80
Joshua De Vera <deverajoshua02@gmail.com>
2025-02-16 00:23:07 +0800

    added protected routes

---
+++ src/routes/ProtectedRoutes.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+// const ProtectedRoute = () => {
+//     const { user } = useAuth();
+//     return user ? <Outlet /> : <Navigate to="/login" replace />;
+// };
+
+const ProtectedRoute = ({ allowedRoles }) => {
+    const { user } = useAuth();
+
+    if (!user) {
+        return <Navigate to="/login" replace />;
+    }
+
+    if (allowedRoles && !allowedRoles.includes(user.role)) {
+        return <Navigate to="/no-access" replace />;
+    }
+
+    return <Outlet />;
+};
+
+export default ProtectedRoute;
