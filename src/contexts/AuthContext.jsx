fcfd33623034c3ef694c25758138b8f94b09a1fb
Joshua De Vera <deverajoshua02@gmail.com>
2025-06-29 23:18:05 +0800

    updated login api endpoitns

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
 
 const AuthContext = createContext();
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api';
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(
