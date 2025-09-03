ab2dd5733490efca77c65a1e86cccf5cc1343b70
Joshua De Vera <deverajoshua02@gmail.com>
2025-09-03 13:10:19 +0800

    improve the graduates demograph

--- src/components/GraduatesLineChart.jsx
+++ src/components/GraduatesLineChart.jsx
@@ -13,25 +13,40 @@ const GraduatesLineChart = ({ data }) => {
   return (
     <ResponsiveContainer width="100%" height={400}>
       <LineChart data={data}>
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="year" />
-        <YAxis allowDecimals={false} />
-        <Tooltip />
-        <Legend />
+        <CartesianGrid stroke="#e0e0e0" strokeDasharray="5 5" />
+        <XAxis
+          dataKey="year"
+          label={{ value: "Year", position: "insideBottom", offset: -5 }}
+        />
+        <YAxis
+          allowDecimals={false}
+          label={{
+            value: "Number of Graduates",
+            angle: -90,
+            position: "insideLeft",
+          }}
+        />
+        <Tooltip
+          formatter={(value, name) => [`${value} graduates`, name]}
+          labelFormatter={(label) => `Year: ${label}`}
+        />
+        <Legend verticalAlign="top" height={36} />
         {/* Current graduates */}
         <Line
-          type="monotone"
+          type="linear"
           dataKey="currentTotal"
-          stroke="#8884d8"
+          stroke="#1f77b4" // blue
           strokeWidth={2}
+          dot={{ r: 4 }}
           name="Current Graduates"
         />
         {/* Previous graduates */}
         <Line
-          type="monotone"
+          type="linear"
           dataKey="previousTotal"
-          stroke="#82ca9d"
+          stroke="#ff7f0e" // orange
           strokeWidth={2}
+          dot={{ r: 4 }}
           name="Previous Graduates"
         />
       </LineChart>
