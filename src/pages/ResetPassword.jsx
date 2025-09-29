16758c73a76f1340eb6df9e5a6cc4b65046d6dbe
Joshua De Vera <deverajoshua02@gmail.com>
2025-09-30 06:07:25 +0800

    updated api shit

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api';
+
 
 const ResetPassword = () => {
   const [searchParams] = useSearchParams();
